c4f655d83f68f11cbd95821bcec01074a9aeeafb
superjojo140 <github@superjojo.de>
2023-01-01 14:56:13 +0100

    Update backend for invoices

--- src/backend/invoice/controller.js
+++ src/backend/invoice/controller.js
@@ -4,16 +4,19 @@ const invoiceService = require("./service");
 
 const router = express.Router({ mergeParams: true }); //mergeParams to access parent router's parameters
 router.post("/create", createInvoice);
+router.get("/list", getInvoiceList);
 router.get("/:invoiceId", getInvoiceSnippets);
 module.exports = router;
 
 async function createInvoice(req, res, next) {
     try {
         const projectId = req.params.projectId;
+        const invoiceId = req.body.invoiceId;
 
         let timestamp = new Date().toISOString();
         timestamp = timestamp.substring(0, timestamp.length - 1);
-        let invoiceId = await invoiceService.createInvoice(timestamp);
+
+        await invoiceService.createInvoice(invoiceId,timestamp,projectId);
         await invoiceService.assignInvoice(projectId, invoiceId);
         res.status(200).json({ message: "Created invoice.", id: invoiceId });
     }
@@ -25,6 +28,15 @@ async function getInvoiceSnippets(req, res, next) {
         const invoiceId = req.params.invoiceId;
         const snippets = await invoiceService.getTimeSnippetsByInvoice(invoiceId);
         res.status(200).json(snippets);
+    }
+    catch (e) { next(e) }
+}
+
+async function getInvoiceList(req, res, next) {
+    try {
+        const projectId = req.params.projectId;
+        const invoices = await invoiceService.getInvoiceList(projectId);
+        res.status(200).json(invoices);
     }
     catch (e) { next(e) }
 }
--- src/backend/invoice/service.js
+++ src/backend/invoice/service.js
@@ -1,12 +1,12 @@
 const dbService = require('./../database/service')
 
-module.exports.assignInvoice = async function (projectId,invoiceId) {
+module.exports.assignInvoice = async function (projectId, invoiceId) {
     const queryCmd = `UPDATE timesnippets SET invoice = '${invoiceId}' WHERE invoice = '0' AND id IN (SELECT timesnippetid FROM projects_timesnippets WHERE projectid='${projectId}');`;
     await dbService.query(queryCmd);
 }
 
-module.exports.createInvoice = async function (timestamp) {
-    const queryCmd = `INSERT INTO invoices (date) VALUES ('${timestamp}')`;
+module.exports.createInvoice = async function (invoiceId, timestamp, projectId) {
+    const queryCmd = `INSERT INTO invoices (id,date,projectId) VALUES ('${invoiceId}','${timestamp}','${projectId}')`;
     const response = await dbService.query(queryCmd);
     return response.insertId;
 }
@@ -15,4 +15,20 @@ module.exports.getTimeSnippetsByInvoice = async function (invoiceId) {
     const queryCmd = `SELECT * FROM timesnippets WHERE invoice = '${invoiceId}' ORDER BY start DESC`;
     let timeSnippetData = await dbService.query(queryCmd);
     return timeSnippetData;
+}
+
+module.exports.getInvoiceList = async function (projectId) {
+    const queryCmd = `SELECT * FROM invoices WHERE projectId = '${projectId}' ORDER BY date DESC`;
+    let invoiceData = await dbService.query(queryCmd);
+
+    for (let invoice of invoiceData) {
+        const queryCmd = `SELECT start FROM timesnippets WHERE invoice = '${invoice.id}' ORDER BY start DESC LIMIT 1`;
+        let result = await dbService.query(queryCmd);
+        invoice.lastStart = result[0].start;
+        const queryCmd2 = `SELECT start FROM timesnippets WHERE invoice = '${invoice.id}' ORDER BY start ASC LIMIT 1`;
+        let result2 = await dbService.query(queryCmd2);
+        invoice.firstStart = result2[0].start;
+    }
+
+    return invoiceData;
 }
