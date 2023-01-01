6f6524082f4890a55d1656c9d8aa5dd62a5a293c
superjojo140 <github@superjojo.de>
2023-01-01 15:27:05 +0100

    invoice API

---
+++ src/frontend/invoice.ts
@@ -0,0 +1,84 @@
+import { TimeSnippet } from "./timesnippet";
+
+export class InvoiceAPI {
+
+    static async getSnippetsByInvoice(projectId: string, invoiceId: string): Promise<TimeSnippet[]> {
+        const jwt = localStorage.getItem("sj_jwt");
+
+        let resp = await fetch(`projects/${projectId}/invoices/${invoiceId}`, {
+            method: 'GET',
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest',
+                'Authorization': `Bearer ${jwt}`
+            }
+        });
+
+        if (!resp.ok) {
+            throw new Error("TimeSnippets could not be fetched.")
+        }
+        let timeSnippetList = await resp.json();
+
+        return timeSnippetList;
+    }
+
+    static async getAll(projectId: string): Promise<Invoice[]> {
+        const jwt = localStorage.getItem("sj_jwt");
+
+        let resp = await fetch(`projects/${projectId}/invoices/list`, {
+            method: 'GET',
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest',
+                'Authorization': `Bearer ${jwt}`
+            }
+        });
+
+        if (!resp.ok) {
+            throw new Error("Invoice List could not be fetched.")
+        }
+        let invoiceList = await resp.json();
+
+        for (let invoiceData of invoiceList) {
+            invoiceData.date = new Date(invoiceData.date);
+            invoiceData.lastStart = new Date(invoiceData.lastStart);
+            invoiceData.firstStart = new Date(invoiceData.firstStart);
+        }
+
+        return invoiceList;
+    }
+
+
+
+
+    static async create(projectId: string, invoiceId: string): Promise<string> {
+        const jwt = localStorage.getItem("sj_jwt");
+
+        let resp = await fetch(`projects/${projectId}/invoices/create`, {
+            method: 'POST',
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest',
+                'Authorization': `Bearer ${jwt}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                invoiceId: invoiceId
+            })
+        });
+
+        if (!resp.ok) {
+            throw new Error("Invoice could not be created.")
+        }
+
+        let data = await resp.json();
+        return data.id;
+    }
+
+
+}
+
+export interface Invoice {
+    id: string,
+    date: Date,
+    projectId: string,
+    lastStart: Date,
+    firstStart: Date
+}
